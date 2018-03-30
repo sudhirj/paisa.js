6428ed891ae76afb2f55aee80413c05e605b83ee
Sudhir Jonathan <sudhir.j@gmail.com>
2018-03-30 16:57:52 +0530

    Moved to ES6

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const SYMBOL = exports.SYMBOL = '₹'
 
-const format = exports.format = function (paise, precision) {
+const format = exports.format = (paise, precision) => {
   if (precision === undefined) {
     precision = 2
   }
@@ -13,25 +13,25 @@ const format = exports.format = function (paise, precision) {
   return stringParts.join('.')
 }
 
-exports.formatWithSymbol = function (paise, precision) {
+exports.formatWithSymbol = (paise, precision) => {
   return [SYMBOL, format(paise, precision)].join('')
 }
 
-exports.words = function (paise) {
+exports.words = (paise) => {
   console.log('WIP')
 }
 
-const parse = function (paise) {
+const parse = (paise) => {
   const littleEndian = paise.toString().split('').reverse()
   const rupeeParts = []
   const paisePart = []
-  Array(2).fill(2).forEach(function () { paisePart.unshift(littleEndian.shift()) })
+  Array(2).fill(2).forEach(() => { paisePart.unshift(littleEndian.shift()) })
   const cycle = [3, 2, 2]
-  var cycleIndex = 0
+  let cycleIndex = 0
   while (littleEndian.length > 0) {
     const part = []
     const cycleSize = cycle[cycleIndex % cycle.length]
-    Array(cycleSize).fill(cycleSize).forEach(function () { part.unshift(littleEndian.shift()) })
+    Array(cycleSize).fill(cycleSize).forEach(() => { part.unshift(littleEndian.shift()) })
     rupeeParts.unshift(part.join(''))
     cycleIndex++
   }
